4bced8ce0ecc1b08646663a6798c43448b1f61cd
Yarden Sapir <yarden@Yardens-MacBook-Pro.local>
2023-10-25 18:40:53 +0300

    add client

---
+++ Client/src/components/Card/Card.tsx
@@ -0,0 +1,18 @@
+
+const Card = () => {
+  return (
+    <div className="card lg:card-side bg-base-100 shadow-xl">
+
+  
+  <div className="card-body">
+    
+    <h2 className="card-title">Task Name</h2>
+    <p>Lorem ipsum doloeque nostrum modi esse cupiditate! Natus id autem illum in blanditiis, architecto qui quibusdam inventore aliquam, tenetur laboriosam!</p>
+    <div className="card-actions justify-end">
+      <button className="btn btn-primary">Learn More</button>
+    </div>
+  </div>
+</div>
+  )
+};
+export default Card;
---
+++ Client/src/components/Footer/Footer.tsx
@@ -0,0 +1,11 @@
+
+const Footer = () => {
+
+
+    return (
+        <footer className=" bg-red-400 flex justify-center items-center h-10">
+            <h1>Joz Ve Loz</h1>
+        </footer>
+    )
+};
+export default Footer;
